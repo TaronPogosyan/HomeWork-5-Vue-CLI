53c69bf09cb0be343a8b2fc7996c3d5772c7b617
taron.poghosyan@yandex.ru <taron.poghosyan@yandex.ru>
2023-09-04 23:40:36 +0300

    Домашняя задания 5 - Фреймворк Vue.js

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <img :src="product.image" />
+    <p>name: {{ product.name }}</p>
+    <p>price: {{ formattedPrice }}</p>
+    <p v-if="product.available">Available</p>
+    <p v-else>Out of stock</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductDetails",
+
+  data() {
+    return {
+      product: {
+        image: require("../assets/nutella.jpg"),
+        name: "Nutella",
+        price: 99.99,
+        available: false,
+      },
+    };
+  },
+
+  computed: {
+    formattedPrice: function () {
+      return `$${this.product.price}`;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+img {
+  width: 25%;
+}
+
+div {
+  display: inline;
+  font-size: 24px;
+}
+</style>
